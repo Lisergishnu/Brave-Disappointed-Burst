82e1f283e4447e10885dc279326102c7d8baa09a
ysentu <rpozop@yahoo.es>
2014-05-25 01:53:00 -0400

    intento de deshabilitar mouse durante simulacion
    no reconoce el metodo declarado en otra clase a pesar de ser public...
    porque pasa esto???

--- src/LabMenuListener.java
+++ src/LabMenuListener.java
@@ -34,8 +34,14 @@ public class LabMenuListener implements ActionListener {
       if (text.equals("Spring")) ;
 
       // Actions associated to MyWorld submenu
-      if (text.equals("Start"))  world.start();
-      if (text.equals("Stop"))    world.stop();
+      if (text.equals("Start")){
+    	  world.start();
+    	  //disableMouseListener();
+      }
+      if (text.equals("Stop")){
+    	  world.stop();
+    	  //enableMouseListener();
+      }
       if (text.equals("Delta time")) {
          String data = JOptionPane.showInputDialog("Enter delta t [s]", "0.0001");
          if (data == null) 
